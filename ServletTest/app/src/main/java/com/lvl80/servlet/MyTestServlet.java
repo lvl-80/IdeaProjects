98c8505f264f7c2f3b340ab7969b8f4584da7178
lvl-80 <me.sudaev.nikita@gmail.com>
2022-07-21 22:48:54 +0300

    haha

--- ServletTest/app/src/main/java/com/lvl80/servlet/MyTestServlet.java
+++ ServletTest/app/src/main/java/com/lvl80/servlet/MyTestServlet.java
@@ -1,14 +1,12 @@
 package com.lvl80.servlet;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/first")
 public class MyTestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- ServletTest/app/src/main/java/com/lvl80/servlet/MyTestServlet2.java
+++ ServletTest/app/src/main/java/com/lvl80/servlet/MyTestServlet2.java
@@ -2,13 +2,11 @@ package com.lvl80.servlet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/second")
 public class MyTestServlet2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -17,7 +15,7 @@ public class MyTestServlet2 extends HttpServlet {
             RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher(path);
             requestDispatcher.forward(request, response);
         } else {
-            response.sendRedirect(request.getContextPath() + "/first");
+            response.sendRedirect(request.getContextPath() + "/haha1");
         }
     }
 }
